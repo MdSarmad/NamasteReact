868698ec8506698ebe3e77fdff0f9ae2a82e1d5e
Md Sarmad <28mdsarmad99@gmail.com>
2022-12-10 23:54:18 +0530

    assignment4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Header from './components/header/Header';
+import Container from './Container';
 
 const App = () => {
-    const header1 = React.createElement("h1", { className: 'header' }, 'Namaste React');
-    const header2 = React.createElement("h2", null, "Namaste JavaScript");
-    const header3 = React.createElement("h3", null, "Namaste everyone");
   return (
     <>
-    <Header/>
-    {
-        React.createElement("div", { className: 'container' }, [header1,header2,header3])
-    }
+      <Header/>
+      <Container />
     </>
   )
 }
